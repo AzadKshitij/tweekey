1df9bc73587b80c1e8c010cc29bbd2799c1600c4
sakhare782 <dattatray.1@iitj.ac.in>
2022-05-14 20:48:47 +0530

    header(navbar)

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="App bg-black min-h-screen flex max-w-[1600px] mx-auto">
+      <div class="flex flex-row w-full h-20 justify-end">
+        <div class="w-56 text-white font-bold pt-4 pl-4 text-3xl bg-green-700 mr-auto">May 2022</div>
+        <div class="w-10 text-white bg-red-800">02</div>
+        <div class="w-10 text-white bg-amber-400">03</div>
+        <div class="w-10 text-white bg-teal-600">04</div>
+        <div class="w-10 text-white bg-lime-400">05</div>
+      </div>
+
+      
+      
     </div>
   );
 }
